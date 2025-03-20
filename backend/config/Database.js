52f7fa3bb71d60755748773205bbccb3a2c34b6b
Arya <aryazakaria67@gmail.com>
2025-03-20 10:34:31 +0700

    ubah db

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
 // Nyambungin db ke BE
-const db = new Sequelize("js", "root", "", {
-  host: "localhost",
+const db = new Sequelize("RECOVER_YOUR_DATA", "root", "", {
+  host: "104.197.117.136",
   dialect: "mysql",
 });
 
